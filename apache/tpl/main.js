1a500cd91e9b1eaa3e89307f9d5262ba9d85df6b
Andrew Molchanov <mr.catmanrus@gmail.com>
2022-06-24 21:50:43 +0300

    Update apache template

--- apache/tpl/main.js
+++ apache/tpl/main.js
@@ -1,43 +1,43 @@
-// By JCat
+// By JoCat
 // Contacts: https://jocat.ru/
 
 "use strict";
 
 // Breadcrumbs
-const breadcrumbs_el = document.querySelector(".breadcrumb");
+const breadcrumbsEl = document.querySelector(".breadcrumb");
 const breadcrumbs = location.pathname
   .split("/")
   .filter((word) => word.length > 0);
 
-breadcrumbs_el.append(get_breadcrumbs_el("/", "Home", breadcrumbs.length == 0));
+breadcrumbsEl.append(getBreadcrumbsEl("/", "Home", breadcrumbs.length === 0));
 if (breadcrumbs.length > 0) {
-  let tree = "";
+  let tree = "/";
   breadcrumbs.forEach((link, index) => {
-    tree += "/" + link;
-    breadcrumbs_el.append(
-      get_breadcrumbs_el(tree, link, index == breadcrumbs.length - 1)
+    tree += link + "/";
+    breadcrumbsEl.append(
+      getBreadcrumbsEl(tree, link, index === breadcrumbs.length - 1)
     );
   });
 }
 
-function get_breadcrumbs_el(link, text, last = false) {
+function getBreadcrumbsEl(link, text, last = false) {
   const el = document.createElement("li");
   el.classList.add("breadcrumb-item");
-  if (last === true) {
+  if (last) {
     el.classList.add("active");
-    el.innerHTML = text;
+    el.innerText = text;
     return el;
   }
   const a = document.createElement("a");
   a.href = link;
-  a.innerHTML = text;
+  a.innerText = text;
   el.append(a);
   return el;
 }
 
 // Toggle content
 document.querySelectorAll(".js__toggle_content").forEach((el) => {
-  if (el.dataset.pathname == location.pathname) el.style.display = "block";
+  if (el.dataset.pathname === location.pathname) el.style.display = "block";
 });
 
 // Pretty table
